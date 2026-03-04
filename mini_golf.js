f5d613af167431c53b7def011de4894d93611f93
Dima-Pizhevskii <24640dp@hvhs.school.nz>
2026-03-04 13:12:02 +0000

    add menu screen title click to start

--- mini_golf.js
+++ mini_golf.js
@@ -15,7 +15,26 @@ function setup() {
 }
 
 function draw() {
+  if (gameState === "menu") {
+    drawMenu();
+  }
+}
+
+function drawMenu() {
   background(34, 100, 34);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  noStroke();
+  textSize(48);
+  text("MINI GOLF", width / 2, height / 2 - 40);
+  textSize(24);
+  text("Click to Start", width / 2, height / 2 + 30);
+}
+
+function mousePressed() {
+  if (gameState === "menu") {
+    gameState = "play";
+  }
 }
 /*******************************************************/
 //  END OF APP
